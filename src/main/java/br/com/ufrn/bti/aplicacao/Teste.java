b2748071523dbd38b16ed0f686c4c54cd6eb2604
inaciomdrs <boyinacio@gmail.com>
2016-11-23 22:38:51 -0300

    ajeita o projeto para trabalhar com banco de dados próprio

--- src/main/java/br/com/ufrn/bti/aplicacao/Teste.java
+++ src/main/java/br/com/ufrn/bti/aplicacao/Teste.java
@@ -1,8 +1,11 @@
 package br.com.ufrn.bti.aplicacao;
 
 import br.com.ufrn.bti.dao.CartaoBancarioDAO;
+import br.com.ufrn.bti.dao.PessoaDAO;
+import br.com.ufrn.bti.dao.UsuarioDAO;
 import br.com.ufrn.bti.dominio.CartaoBancario;
 import br.com.ufrn.bti.dominio.Pessoa;
+import br.com.ufrn.bti.dominio.Usuario;
 
 public class Teste {
 
@@ -13,10 +16,22 @@ public class Teste {
 	public static void main(String[] args) {
 		Pessoa pessoa = new Pessoa();
 		pessoa.setCpf("12345678910");
+		pessoa.setNome("Inácio Medeiros");
 		
-		CartaoBancario cartao = new CartaoBancarioDAO().getCartaoDa(pessoa);
+		Usuario usuario = new Usuario();
+		usuario.setLogin("boyinacio");
+		usuario.setSenha("123");
+		usuario.setPessoa(pessoa);
 		
-		System.out.println(cartao.getLimite());
+		CartaoBancario cartao = new CartaoBancario(pessoa, 123, 0, 2000);
+		
+		new PessoaDAO().salvarOuAtualizar(pessoa);
+		new UsuarioDAO().salvarOuAtualizar(usuario);
+		new CartaoBancarioDAO().salvarOuAtualizar(cartao);
+		
+		CartaoBancario card = new CartaoBancarioDAO().getCartaoDa(pessoa);
+		
+		System.out.println(card.getLimite());
 	}
 
 }
